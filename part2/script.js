c0a51e31224bc4b5e73075ce55a9e9fed7566514
Julliane Pamfilo <pamfilojulliane@yahoo.com>
2023-02-03 22:27:02 -0600

    Likes

---
+++ part2/script.js
@@ -0,0 +1,33 @@
+var count = 9;
+var like1 = document.querySelector("#click");
+
+console.log(like1);
+
+function add1() {
+    count++;
+    like1.innerText = count + " like(s)" ;
+    console.log(count);
+}
+
+var count2 = 12;
+var like2 = document.querySelector("#click2");
+
+console.log(count2);
+
+function add2() {
+    count2++;
+    like2.innerText = count2 + " like(s)";
+    console.log(count2);
+}
+
+var count3 = 9;
+var like3 = document.querySelector("#click3");
+
+console.log(like3);
+
+function add3() {
+    count3++;
+    like3.innerText = count3 + " like(s)";
+    console.log(count3);
+}
+
